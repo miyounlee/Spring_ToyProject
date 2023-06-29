7985138e03f4b9442a177f269481e157c8abe05b
YangSooHyun0 <111266513+YangSooHyun0@users.noreply.github.com>
2023-06-29 18:01:18 +0900

    OutPlayer 구현

--- ToyProject/src/main/java/model/OutPlayer.java
+++ ToyProject/src/main/java/model/OutPlayer.java
@@ -1,2 +1,14 @@
-package model;public class OutPlayer {
+package model;
+
+import java.sql.Timestamp;
+import lombok.*;
+@AllArgsConstructor
+@ToString
+@Builder
+@Getter
+public class OutPlayer {
+    private int id;
+    private int playerId;
+    private String reason;
+    private Timestamp createdAt;
 }
