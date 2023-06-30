cad15fca3a141e9c1a501aad0f5701cae28bfd9d
Miyeon Lee <leeyun92@naver.com>
2023-06-30 13:35:30 +0900

    feat : 입력 파싱 클래스 추가

--- src/main/java/BaseBallApp.java
+++ src/main/java/BaseBallApp.java
@@ -1,6 +1,12 @@
-
+import dao.PlayerDAO;
+import dao.PlayersByPositionDAO;
+import dao.StadiumDAO;
+import dao.TeamDAO;
 import db.DBConnection;
-
+import handler.RequestHandler;
+import service.PlayerService;
+import service.StadiumService;
+import service.TeamService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,7 +17,28 @@ public class BaseBallApp {
 
         Connection connection = DBConnection.getInstance();
 
+        // ---------- dao ----------
+        TeamDAO teamDAO = TeamDAO.getInstance();
+        teamDAO.setConnection(connection);
 
+        StadiumDAO stadiumDAO = StadiumDAO.getInstance();
+        stadiumDAO.setConnection(connection);
+
+        PlayerDAO playerDAO = PlayerDAO.getInstance();
+        playerDAO.setConnection(connection);
+
+        PlayersByPositionDAO playersByPositionDAO = PlayersByPositionDAO.getInstance();
+        playersByPositionDAO.setConnection(connection);
+
+        // ---------- service ----------
+
+        StadiumService stadiumService = new StadiumService(stadiumDAO, connection);
+        TeamService teamService = new TeamService(teamDAO, connection);
+        PlayerService playerService = new PlayerService(playerDAO, playersByPositionDAO, connection);
+        RequestHandler requestHandler = new RequestHandler(stadiumService, teamService, playerService);
+
+        // ---------- 파싱 ----------
+        requestHandler.requestHandler();
 
     }
 }
---
+++ src/main/java/handler/RequestInfo.java
@@ -0,0 +1,17 @@
+package handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestInfo {
+    private String name;
+    private Map<String, Object> parameterMap;
+}
